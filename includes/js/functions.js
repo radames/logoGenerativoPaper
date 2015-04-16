73f6ae6c51672ab415f5cf54f381fdbe0e927b17
Radamés Ajna <radamajna@gmail.com>
2015-04-16 17:27:22 -0500

    spaces

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -27,6 +27,24 @@ $(document).ready(function(){
 		$("#fs-type-save option[value*='fs-security']").show();
 		$("#fs-type-save option[value*='fs-assistance']").show();
 		$("#fs-type-save option[value*='fs-insurance']").show();
-
 	});
+	
+		$("#vertical-gerado").click(function(){
+		$("#fs-type-save option[value*='fs-entertainment']").hide();		
+		$("#fs-type-save option[value*='fs-learning']").hide();		
+		$("#fs-type-save option[value*='fs-security']").hide();			
+		$("#fs-type-save option[value*='fs-assistance']").hide();			
+		$("#fs-type-save option[value*='fs-insurance']").hide();
+	});
+								
+	$("#horizontal-gerado").click(function(){
+		$("#fs-type-save option[value*='fs-entertainment']").show();
+		$("#fs-type-save option[value*='fs-learning']").show();
+		$("#fs-type-save option[value*='fs-security']").show();
+		$("#fs-type-save option[value*='fs-assistance']").show();
+		$("#fs-type-save option[value*='fs-insurance']").show();
+	});
+	
+	
+	
 });
